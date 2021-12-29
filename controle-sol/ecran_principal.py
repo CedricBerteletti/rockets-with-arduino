358889b3a75030665fadc2d482b665689e69c0c8
Cédric Berteletti <cedric.berteletti@free.fr>
2021-12-30 00:28:27 +0100

    CCT : layout interface

--- controle-sol/ecran_principal.py
+++ controle-sol/ecran_principal.py
@@ -1,40 +1,60 @@
 # -*- coding: utf-8 -*-
 
-from tkinter import Tk, Text, BOTH, W, N, E, S
-from tkinter.ttk import Frame, Button, Label, Style
+from tkinter import Tk, Text, BOTH, HORIZONTAL, RAISED, W, N, E, S
+from tkinter import Frame, Button, Label, LabelFrame, PanedWindow
+from tkinter.constants import VERTICAL
+from graphique import Graphique
 
 
 class EcranPrincipal(Frame):
 
     def __init__(self):
         super().__init__()
+        self.init_ui()
 
-        self.initUI()
 
-
-    def initUI(self):
+    def init_ui(self):
+        # Préparation de l'écran
         self.pack(fill=BOTH, expand=True)
+        Tk.update(self)
+        panneaux = PanedWindow(self, orient=HORIZONTAL, sashrelief = RAISED, sashpad=2)
+        panneaux.pack(fill=BOTH, expand=True)
 
-        self.columnconfigure(1, weight=1)
-        self.columnconfigure(3, pad=7)
-        self.rowconfigure(3, weight=1)
-        self.rowconfigure(5, pad=7)
+        # Panneau de gauche
+        panneauGauche = PanedWindow(panneaux, orient=VERTICAL, sashrelief = RAISED, sashpad=2, width=self.winfo_width()/2)
+        panneaux.add(panneauGauche)        
 
-        lbl = Label(self, text="Windows")
-        lbl.grid(sticky=W, pady=4, padx=5)
+        # 2 panneaux horizontaux de gauche
+        visualisation = LabelFrame(panneaux, text="Vue 3D")
+        panneauGauche.add(visualisation, height=self.winfo_height()*3/4)
+        controle = LabelFrame(panneaux, text="Contrôle")
+        panneauGauche.add(controle, height=self.winfo_height()/4)
 
-        area = Text(self)
-        area.grid(row=1, column=0, columnspan=2, rowspan=4,
-            padx=5, sticky=E+W+S+N)
+        
 
-        abtn = Button(self, text="Activate")
-        abtn.grid(row=1, column=3)
+        # Panneau de droite contenant les informations de trajectoire
+        panneauDroit = LabelFrame(panneaux, text="Centrale inertielle", width=self.winfo_width()/2)
+        panneaux.add(panneauDroit)
+        panneauDroit.columnconfigure(0, weight=1)
+        panneauDroit.rowconfigure(0, weight=1)
+        panneauDroit.rowconfigure(1, weight=1)
+        panneauDroit.rowconfigure(2, weight=1)
+        panneauDroit.rowconfigure(3, weight=1)
+        panneauDroit.rowconfigure(4, weight=1)
+        panneauDroit.rowconfigure(5, weight=1)
+        panneauDroit.rowconfigure(6, weight=1)
 
-        cbtn = Button(self, text="Close")
-        cbtn.grid(row=2, column=3, pady=4)
-
-        hbtn = Button(self, text="Help")
-        hbtn.grid(row=5, column=0, padx=5)
-
-        obtn = Button(self, text="OK")
-        obtn.grid(row=5, column=3)
+        graph1 = Graphique(panneauDroit, text="x")
+        graph1.grid(column=0, row=0, sticky=(N, S, E, W))
+        graph2 = Graphique(panneauDroit, text="y")
+        graph2.grid(column=0, row=1, sticky=(N, S, E, W))
+        graph3 = Graphique(panneauDroit, text="z")
+        graph3.grid(column=0, row=2, sticky=(N, S, E, W))
+        graph4 = Graphique(panneauDroit, text="thetax")
+        graph4.grid(column=0, row=3, sticky=(N, S, E, W))
+        graph5 = Graphique(panneauDroit, text="thetay")
+        graph5.grid(column=0, row=4, sticky=(N, S, E, W))
+        graph6 = Graphique(panneauDroit, text="thetaz")
+        graph6.grid(column=0, row=5, sticky=(N, S, E, W))
+        imuLogs = Text(panneauDroit, bg="#000000", fg="#FFFFFF")
+        imuLogs.grid(column=0, row=6, sticky=(N, S, E, W))
---
+++ controle-sol/graphique.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from tkinter import Tk, Text, BOTH, HORIZONTAL, RAISED, W, N, E, S
+from tkinter import Frame, Button, Label, LabelFrame, PanedWindow
+from tkinter.constants import VERTICAL
+
+
+class Graphique(LabelFrame):
+
+    def __init__(self, root, text):
+        super().__init__(root, text=text)
+        self.init_ui()
+
+    def init_ui(self):
+        area = Text(self)
+        area.pack(fill=BOTH, expand=True)
