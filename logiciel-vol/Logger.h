3f45e4b6a1d6903d3215b470fdfdd400731ef64a
CedricBerteletti <47115606+CedricBerteletti@users.noreply.github.com>
2022-01-01 12:43:34 +0100

    [logiciel-vol] Logger sur la carte SD

--- logiciel-vol/Logger.h
+++ logiciel-vol/Logger.h
@@ -8,16 +8,26 @@
 #include "Arduino.h"
 #include "aides.h"
 #include "Wifi.h"
+#include <SD.h>
 
 class Logger
 {
   public:
     Logger();
+    void initSdcard();
     void log(String module, String message, String details);
+    void flush();
     bool toSerial = true;
     bool toSdcard = false;
     bool toUdp = false;
     Wifi *wifi;
+  private:
+    static const String MODULE_LOGGER;
+    static const String NOM_FICHIER_LOGS;
+    Sd2Card carteSd;
+    SdVolume volume;
+    SdFile root;
+    File fichier;
 };
 
 #endif
