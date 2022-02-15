9050ec0a9bf4e8f433d33bca21dded56f31b92d6
CedricBerteletti <47115606+CedricBerteletti@users.noreply.github.com>
2022-02-15 20:52:24 +0100

    [controle-sol] Filtrage des données des gyroscopes pour éviter la dérive
    Egalement correction de la calibration

--- controle-sol/centrale_inertielle.py
+++ controle-sol/centrale_inertielle.py
@@ -7,84 +7,92 @@ Calculs à partir des données de la centrale inertielle
 from dataclasses import dataclass
 
 
+FILTRE_VITESSE_ANGULAIRE_MIN = 0.2 # 0.1 ° par seconde
+FILTRE_ACCELERATION_MIN = 0.001 # g (gravité terrestre 9.8 m/s²)
+
 class CentraleInertielle():
     "Classe gérant toutes les données inertielles"
 
     def __init__(self):
         self.courant = DonneesInertielles()
-        print(self.courant)
         self.data_liste = []
-        self.deltaax = 0.0
-        self.deltaay = 0.0
-        self.deltaaz = 0.0
-        self.deltavalpha = 0.0
-        self.deltavbeta = 0.0
-        self.deltavgamma = 0.0
+        self.delta_ax = 0.0
+        self.delta_ay = 0.0
+        self.delta_az = 0.0
+        self.delta_valpha = 0.0
+        self.delta_vbeta = 0.0
+        self.delta_vgamma = 0.0
         
     def ajouter_telemetrie(self, log):
-        if(log):
+        if (log):
             data = DonneesInertielles()
             tokens = log.split("\", \"")
             if(len(tokens[1]) > 1):
                 values = tokens[1][:-1].split(", ")
+
                 # Récupération des valeurs de la télémétrie, en tenant compte de la calibration
                 data.t = int(values[0])
-                data.ax = float(values[1]) + self.deltaax
-                data.ay = float(values[2]) + self.deltaay
-                data.az = float(values[3]) + self.deltaaz
-                data.valpha = float(values[4]) + self.deltavalpha
-                data.vbeta = float(values[5]) + self.deltavbeta
-                data.vgamma = float(values[6]) + self.deltavgamma
+                data.ax = float(values[1]) + self.delta_ax
+                data.ay = float(values[2]) + self.delta_ay
+                data.az = float(values[3]) + self.delta_az
+                data.valpha = float(values[4]) + self.delta_valpha
+                data.vbeta = float(values[5]) + self.delta_vbeta
+                data.vgamma = float(values[6]) + self.delta_vgamma
 
-                # Calcul de l'orientation et de la position
-                temps_ecoule = float(data.t - self.courant.t) / 1000.0
-                print(data.alpha, self.courant.alpha, data.valpha)
-                data.alpha = self.courant.alpha + data.valpha * temps_ecoule
-                data.beta = self.courant.beta + data.vbeta * temps_ecoule
-                data.gamma = self.courant.gamma + data.vgamma * temps_ecoule
+                # Pour éviter la dérive des gyroscopes, on filtre les trop petites vitesses angulaires
+                if (abs(data.valpha) < FILTRE_VITESSE_ANGULAIRE_MIN):
+                    data.valpha = 0.0
+                if (abs(data.vbeta) < FILTRE_VITESSE_ANGULAIRE_MIN):
+                    data.vbeta = 0.0
+                if (abs(data.vgamma) < FILTRE_VITESSE_ANGULAIRE_MIN):
+                    data.vgamma = 0.0
+
+                if (self.courant.t > 0):
+                    # Il ne s'agit pas de la première donnée reçue, on peut donc calculer
+                    # l'orientation et la position
+                    temps_ecoule = float(data.t - self.courant.t) / 1000.0
+                    data.alpha = self.courant.alpha + data.valpha * temps_ecoule
+                    data.beta = self.courant.beta + data.vbeta * temps_ecoule
+                    data.gamma = self.courant.gamma + data.vgamma * temps_ecoule
 
                 # Mise à jour de la liste
                 self.courant = data
                 self.data_liste.append(data)
 
     def effacer_donnees(self):
+        "Efface toutes les données stockées, mais ne modifie pas la calibration"
         self.data_liste = []
         self.courant = DonneesInertielles()
-        self.deltaax = 0.0
-        self.deltaay = 0.0
-        self.deltaaz = 0.0
-        self.deltavalpha = 0.0
-        self.deltavabeta = 0.0
-        self.deltavagamma = 0.0
 
     def calibrer(self):
-        self.deltaax = 0.0
-        self.deltaay = 0.0
-        self.deltaaz = 0.0
-        self.deltavalpha = 0.0
-        self.deltavabeta = 0.0
-        self.deltavagamma = 0.0
+        "Calibre les accéléromètres et les gyroscopes par rapport aux données stockées, mais ne modifie pas ces dernières"
+        self.delta_ax = 0.0
+        self.delta_ay = 0.0
+        self.delta_az = 0.0
+        self.delta_valpha = 0.0
+        self.delta_vbeta = 0.0
+        self.delta_vgamma = 0.0
         n = 0
         
         # Calcul de la déviation sur tous les échantillons
         for data in self.data_liste:
-            self.deltaax = self.deltaax + data.ax
-            self.deltaay = self.deltaay + data.ay
-            self.deltaaz = self.deltaaz + data.az
-            self.deltavalpha = self.deltavalpha + data.valpha
-            self.deltavbeta = self.deltavbeta + data.vbeta
-            self.deltavgamma = self.deltavgamma + data.vgamma
+            self.delta_ax = self.delta_ax + data.ax
+            self.delta_ay = self.delta_ay + data.ay
+            self.delta_az = self.delta_az + data.az
+            self.delta_valpha = self.delta_valpha + data.valpha
+            self.delta_vbeta = self.delta_vbeta + data.vbeta
+            self.delta_vgamma = self.delta_vgamma + data.vgamma
             n = n+1
         
         # Déviation moyenne en fonction du nombre n d'échantillons
-        self.deltaax = - self.deltaax / n
-        self.deltaay = - self.deltaay / n
-        self.deltaaz = - self.deltaaz / n
-        self.deltavalpha = - self.deltavalpha / n
-        self.deltavbeta = - self.deltavbeta / n
-        self.deltavgamma = - self.deltavgamma / n
+        self.delta_ax = - self.delta_ax / n
+        self.delta_ay = - self.delta_ay / n
+        self.delta_az = - self.delta_az / n
+        self.delta_valpha = - self.delta_valpha / n
+        self.delta_vbeta = - self.delta_vbeta / n
+        self.delta_vgamma = - self.delta_vgamma / n
 
-        print("CALIBRATION ", self.deltaax, self.deltaay, self.deltaaz, self.deltavalpha, self.deltavabeta, self.deltavagamma)
+        print("CALIBRATION ", self.delta_ax, self.delta_ay, self.delta_az, self.delta_valpha, self.delta_vbeta, self.delta_vgamma)
 
 
 
