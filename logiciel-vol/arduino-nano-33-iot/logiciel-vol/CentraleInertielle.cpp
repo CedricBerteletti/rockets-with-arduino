7d228c6fbf6668fc419d8e6e047de62d622c6a1e
CedricBerteletti <47115606+CedricBerteletti@users.noreply.github.com>
2024-06-09 12:35:02 +0200

    Programmes de tests

--- logiciel-vol/arduino-nano-33-iot/logiciel-vol/CentraleInertielle.cpp
+++ logiciel-vol/arduino-nano-33-iot/logiciel-vol/CentraleInertielle.cpp
@@ -37,8 +37,9 @@ void CentraleInertielle::lire() {
   if (IMU.accelerationAvailable() && IMU.gyroscopeAvailable()) {
     donneesInertiellesFrenetCourantes.t = millis();
     donneesInertiellesFrenetPrecedentes = donneesInertiellesFrenetCourantes;
-    IMU.readAcceleration(donneesInertiellesFrenetCourantes.accX, donneesInertiellesFrenetCourantes.accY, donneesInertiellesFrenetCourantes.accZ);
-    IMU.readGyroscope(donneesInertiellesFrenetCourantes.vAlpha, donneesInertiellesFrenetCourantes.vBeta, donneesInertiellesFrenetCourantes.vGamma);
+    // Adaptation à la position de montage de l'Arduino dans la fusée
+    IMU.readAcceleration(donneesInertiellesFrenetCourantes.accZ, donneesInertiellesFrenetCourantes.accY, donneesInertiellesFrenetCourantes.accX);
+    IMU.readGyroscope(donneesInertiellesFrenetCourantes.vGamma, donneesInertiellesFrenetCourantes.vBeta, donneesInertiellesFrenetCourantes.vAlpha);
 
     //On calibre et on filtre les valeurs
     donneesInertiellesFrenetCourantes.accX += offsetAccX;
